99d73c95d9cd6a4154d328b19ecb3790e47f7abc
snow <rhasya@gmail.com>
2024-08-20 16:19:51 +0900

    move db url to .env

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,7 @@
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 
-const pwd = encodeURI("rootp@ssw0rd$");
-const client = postgres(`postgres://postgres:${pwd}@localhost:5432/postgres`);
+const client = postgres(process.env.DB_URL!);
 const db = drizzle(client);
 
 export { db };
